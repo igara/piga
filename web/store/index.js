b4f995efdec9df63b42dec8872940512b30d00fe
Syo Igarashi <igara1119@gmail.com>
2019-01-14 01:22:00 +0900

    cookie

--- web/store/index.js
+++ web/store/index.js
@@ -1,4 +1,3 @@
-import CookieParser from 'cookieparser'
 import axios from 'axios'
 
 export const state = () => ({
@@ -13,46 +12,40 @@ export const mutations = {
 
 export const actions = {
   async nuxtServerInit({ commit }, { req }) {
-    const cookies = CookieParser.parse(req.headers.cookie)
-    if (cookies.hasOwnProperty('connect.sid')) {
-      const sessionId = cookies['connect.sid']
-      if (sessionId) {
-        try {
-          const fetchAdmin = await this.$axios.$post(
-            `${process.env.SYONET_URL}/api/auth/admin/check`,
-            {},
-            {
-              headers: { token: `connect.sid=${sessionId}` }
-            }
-          )
-          if (fetchAdmin.status === 200 && fetchAdmin.message === 'OK') {
-            commit('setIsAdmin', true)
+    const sessionId = this.$cookies.get('connect.sid')
+    if (sessionId) {
+      try {
+        const fetchAdmin = await this.$axios.$post(
+          `${process.env.SYONET_URL}/api/auth/admin/check`,
+          {},
+          {
+            headers: { token: `connect.sid=${sessionId}` }
           }
-        } catch (error) {
-          console.log(error)
+        )
+        if (fetchAdmin.status === 200 && fetchAdmin.message === 'OK') {
+          commit('setIsAdmin', true)
         }
+      } catch (error) {
+        console.log(error)
       }
     }
   },
   async getIsAdmin({ commit }) {
-    const cookies = CookieParser.parse(req.headers.cookie)
-    if (cookies.hasOwnProperty('connect.sid')) {
-      const sessionId = cookies['connect.sid']
-      if (sessionId) {
-        try {
-          const fetchAdmin = await this.$axios.$post(
-            `${process.env.SYONET_URL}/api/auth/admin/check`,
-            {},
-            {
-              headers: { token: `connect.sid=${sessionId}` }
-            }
-          )
-          if (fetchAdmin.status === 200 && fetchAdmin.message === 'OK') {
-            commit('setIsAdmin', true)
+    const sessionId = this.$cookies.get('connect.sid')
+    if (sessionId) {
+      try {
+        const fetchAdmin = await this.$axios.$post(
+          `${process.env.SYONET_URL}/api/auth/admin/check`,
+          {},
+          {
+            headers: { token: `connect.sid=${sessionId}` }
           }
-        } catch (error) {
-          console.log(error)
+        )
+        if (fetchAdmin.status === 200 && fetchAdmin.message === 'OK') {
+          commit('setIsAdmin', true)
         }
+      } catch (error) {
+        console.log(error)
       }
     }
   }
--- web/store/iot.js
+++ web/store/iot.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import CookieParser from 'cookieparser'
 
 export const state = () => ({
   devices: []
