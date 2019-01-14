16059c09fc59dd0341e0488e90aba295f3a05467
Syo Igarashi <igara1119@gmail.com>
2019-01-14 16:26:35 +0900

    IPに変更

--- web/server/api/iot.js
+++ web/server/api/iot.js
@@ -5,13 +5,9 @@ const axios = require('axios')
 
 const app = express()
 const client = new Client()
-const ipRegexp = /\d+.\d+.\d+.\d+/
-const macRegexp = /\S+:\S+:\S+:\S+/
 
 app.get('/', async (req, res) => {
-  const tpLinkMacs = process.env.TPLINK_MACS.split(',')
-  const execArp = execSync('arp -a').toString()
-  const arpLine = execArp.split('\n')
+  const tpLinkIps = process.env.TPLINK_IPS.split(',')
 
   const token = req.headers.token
   if (token) {
@@ -40,21 +36,15 @@ app.get('/', async (req, res) => {
       })
     }
   }
-  const arps = {}
   const devices = []
   try {
-    for (const line of arpLine) {
-      const macMatch = line.match(macRegexp)
-      if (macMatch && macMatch.length > 0 && tpLinkMacs.includes(macMatch[0])) {
-        const ipMatch = line.match(ipRegexp)
-        const ip = ipMatch[0]
-        const device = await client.getDevice({ host: ip })
-        const sysInfo = await device.getSysInfo()
-        const powerState = await device.getPowerState()
-        sysInfo.power_state = powerState
-        sysInfo.ip = ip
-        devices.push(sysInfo)
-      }
+    for (const ip of tpLinkIps) {
+      const device = await client.getDevice({ host: ip })
+      const sysInfo = await device.getSysInfo()
+      const powerState = await device.getPowerState()
+      sysInfo.power_state = powerState
+      sysInfo.ip = ip
+      devices.push(sysInfo)
     }
   } catch (error) {
     console.error(error)
@@ -70,9 +60,7 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
-  const tpLinkMacs = process.env.TPLINK_MACS.split(',')
-  const execArp = execSync('arp -a').toString()
-  const arpLine = execArp.split('\n')
+  const tpLinkIps = process.env.TPLINK_IPS.split(',')
 
   const token = req.headers.token
   if (token) {
@@ -114,24 +102,18 @@ app.post('/', async (req, res) => {
   }
   const ipAddress = req.body['ip']
   const turn = req.body['turn']
-  const arps = {}
   const devices = []
   try {
-    for (const line of arpLine) {
-      const macMatch = line.match(macRegexp)
-      if (macMatch && macMatch.length > 0 && tpLinkMacs.includes(macMatch[0])) {
-        const ipMatch = line.match(ipRegexp)
-        const ip = ipMatch[0]
-        const device = await client.getDevice({ host: ip })
-        if (ip === ipAddress) {
-          await device.setPowerState(turn)
-        }
-        const sysInfo = await device.getSysInfo()
-        const powerState = await device.getPowerState()
-        sysInfo.power_state = powerState
-        sysInfo.ip = ip
-        devices.push(sysInfo)
+    for (const ip of tpLinkIps) {
+      if (tpLinkIps === ipAddress) {
+        await device.setPowerState(turn)
       }
+      const device = await client.getDevice({ host: ip })
+      const sysInfo = await device.getSysInfo()
+      const powerState = await device.getPowerState()
+      sysInfo.power_state = powerState
+      sysInfo.ip = ip
+      devices.push(sysInfo)
     }
   } catch (error) {
     console.error(error)
